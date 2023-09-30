f527de2f0dbb309898d94fcf532fecc00b62de09
Dao Tuyet Ngan (Peachy) <107946882+Peachy72@users.noreply.github.com>
2023-09-30 14:13:48 +0700

    docs(TermsAndConditionsScreen.kt): update terms and conditions docs

--- app/src/main/java/com/example/ecoearn/screens/TermsAndConditionsScreen.kt
+++ app/src/main/java/com/example/ecoearn/screens/TermsAndConditionsScreen.kt
@@ -1,8 +1,11 @@
 package com.example.ecoearn.screens
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -12,6 +15,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.ecoearn.R
 import com.example.ecoearn.components.HeadingTextComponent
+import com.example.ecoearn.components.NormalTextComponent
 import com.example.ecoearn.navigation.EcoEarnAppRouter
 import com.example.ecoearn.navigation.Screen
 import com.example.ecoearn.navigation.SystemBackButtonHandler
@@ -23,7 +27,19 @@ fun TermsAndConditionsScreen() {
         .background(color = Color.White)
         .padding(16.dp)
     ) {
-        HeadingTextComponent(value = stringResource(id = R.string.terms_and_conditions_header))
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .verticalScroll(rememberScrollState())
+        ) {
+            HeadingTextComponent(value = stringResource(id = R.string.terms_and_conditions_header))
+            NormalTextComponent(value = "1. Acceptance of Terms By accessing and using this website (the “Site”), you accept and agree to be bound by the terms and provisions of this agreement (the “Terms”). In addition, when using the Site’s particular services, you shall be subject to any posted guidelines or rules applicable to such services.",)
+            NormalTextComponent(value = "2. Modification of Terms We reserve the right, at our sole discretion, to modify or replace these Terms at any time. If a revision is material we will provide at least 30 days’ notice prior to any new terms taking effect.")
+            NormalTextComponent(value = "3. User Conduct You agree to use the Site only for lawful purposes. You agree not to take any action that might compromise the security of the Site, render the Site inaccessible to others, or otherwise cause damage to the Site or its content.")
+            NormalTextComponent(value = "4. Intellectual Property All intellectual property rights in the Site and its content (including without limitation trademarks and copyright) are owned by us or our licensors.")
+            NormalTextComponent(value = "5. Limitation of Liability In no event will we be liable for any direct, indirect, special, consequential damages or any other damages whatsoever arising out of the use of or inability to use the Site.")
+            NormalTextComponent(value = "6. Governing Law These Terms shall be governed by and construed in accordance with the laws of [Your Jurisdiction].")
+        }
     }
     SystemBackButtonHandler {
         EcoEarnAppRouter.navigateTo(Screen.SignUpScreen)
