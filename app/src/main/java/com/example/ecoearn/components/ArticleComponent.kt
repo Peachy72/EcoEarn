dc5d8649c0aa7efb09a4d50fd9fe9dfa944d8105
Dao Tuyet Ngan (Peachy) <107946882+Peachy72@users.noreply.github.com>
2023-10-01 09:58:24 +0700

    feat: homeScreen implementation

--- app/src/main/java/com/example/ecoearn/components/ArticleComponent.kt
+++ app/src/main/java/com/example/ecoearn/components/ArticleComponent.kt
@@ -1,8 +1,60 @@
 package com.example.ecoearn.components
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.ecoearn.ui.theme.Primary
+import com.example.ecoearn.ui.theme.Secondary
 
 @Composable
-fun ArticleComponent() {
+fun ArticleComponent(modifier: Modifier = Modifier) {
+    Column(
+        modifier = Modifier
+            .padding(8.dp)
+            .background(Secondary)
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(200.dp)
+                .background(Primary)
+        )
+        Text(
+            text = "Article Title: Lorem ipsum dolor sit amet",
+            style = TextStyle(
+                fontSize = 18.sp,
+                fontWeight = FontWeight.Bold,
+                fontStyle = FontStyle.Normal
+            ),
+            modifier = Modifier.padding(
+                start = 8.dp,
+                end = 8.dp,
+            )
+        )
+        Text(
+            text = "Article Description: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+            style = TextStyle(
+                fontSize = 14.sp,
+                fontWeight = FontWeight.Normal,
+                fontStyle = FontStyle.Normal
+            ),
+            modifier = Modifier.padding(
+                start = 8.dp,
+                end = 8.dp,
+                bottom = 16.dp
+            )
+        )
+    }
 
 }
--- app/src/main/java/com/example/ecoearn/screens/HomeScreen.kt
+++ app/src/main/java/com/example/ecoearn/screens/HomeScreen.kt
@@ -2,7 +2,10 @@ package com.example.ecoearn.screens
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -13,21 +16,33 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.ecoearn.components.ArticleComponent
+import com.example.ecoearn.components.BottomBarComponent
+import com.example.ecoearn.components.SeeMoreComponent
 import com.example.ecoearn.ui.theme.Primary
+import com.example.ecoearn.ui.theme.TextColor
 
 @Composable
 fun HomeScreen() {
-    Column {
-        Spacer(modifier = Modifier.height(100.dp))
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+    ) {
+        Spacer(modifier = Modifier.height(16.dp))
         Text(
             text = "News",
             style = TextStyle(
-                fontSize = 18.sp,
+                fontSize = 32.sp,
                 fontWeight = FontWeight.Bold,
                 fontStyle = FontStyle.Normal
         ),
-        color = Primary,
+        color = TextColor,
         textAlign = TextAlign.Center
         )
+        repeat(2) {
+            ArticleComponent()
+        }
+        SeeMoreComponent {}
+        BottomBarComponent()
     }
 }
